780df66401cc0d2f933a7af920a4460b189ea219
Isabella Firmino <isabella.mfbr@gmail.com>
2021-10-22 05:21:16 -0400

    Final Version

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,5 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+alert("Seja bem vindo! Apenas o botão 'Mais informações' é clicável para mostrar detalhes da série. Gostaria de ter feito algo mais detalhado, mas acredito que tenham maneiras menos trabalhosas de fazê-lo. Espero que aproveite a experiência.")
